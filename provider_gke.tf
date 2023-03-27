1297eef2a404cb184913cf54032e6b50bf1f96a6
Ryan Faircloth <ryan@dss-i.com>
2023-03-27 13:44:23 -0400

    fix: fix provider

--- provider_gke.tf
+++ provider_gke.tf
@@ -2,7 +2,7 @@ module "gke_auth" {
   source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
   version      = "24.1.0"
   project_id   = var.project_id
-  location     = var.location
+  location     = var.region
   cluster_name = var.cluster_name
 }
 data "google_client_config" "default" {}
